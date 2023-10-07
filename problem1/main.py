de71f09a6bd4d8aad6169ae19b91fd789f69cff4
Karnopunta <145122800+Karnopunta@users.noreply.github.com>
2023-10-07 14:29:45 +0700

    main.py
    menulis solusi

--- problem1/main.py
+++ problem1/main.py
@@ -2,5 +2,15 @@
 student_score = 80
 
 # Process: Your Solution Code Here
+if 80 <= student_score <= 100 :
+   print('nilai A ')
+elif 65 <= student_score <= 79 :
+   print('nilai B+ ')
+elif 50 <= student_score <= 64 :
+   print('nilai B ')
+elif 35 <= student_score <= 49 :
+   print('nilai C ')
+else :
+   print('nilai D ')
 
 # Output "Nilai A"
